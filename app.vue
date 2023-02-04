fffaed58ffdf37a3f84ecab95635e042900aa9be
Daniele Tortora <tortora87@gmail.com>
2023-02-04 11:10:42 +0100

    start template with env vars example and eslint prettier setup

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup>
+const runtime = useRuntimeConfig();
+console.log(runtime);
+</script>
+
+<template>
+  <div>
+    <code>
+      <pre>{{ runtime }}</pre>
+    </code>
+  </div>
+</template>
